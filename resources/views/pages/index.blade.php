9d857720bf14c0fc21aed02343e5b9cbfa454266
Guddu Kumar Ray <gudduray9869@gmail.com>
2025-02-12 15:41:27 +0530

    index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,62 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  @include('include.css');
+  @include('include.css')
 </head>
 <body>
   
-  <!-------Start header section------->  
- @include('include.header');
-<!-------Emd header section-\------>
+  <!------- Start header section ------->
+  @include('include.header')
+  <!------- End header section -------->
 
-<!------Star home banner section------->
-@include('include.home-banner');
-<!------End home banner section------->
+  <!------ Start home banner section ------>
+  @include('include.home-banner')
+  <!------ End home banner section ------>
 
-<!------Start buy business section------->
-@include('include.buy-business');
-<!------End buy business section------->
+  <!------ Start buy business section ------>
+  @include('include.buy-business')
+  <!------ End buy business section ------>
 
-<!--------Start benefits section------->
-@include('include.benefits');
-<!--------End benefits section--------->
+  <!-------- Start benefits section -------->
+  @include('include.benefits')
+  <!-------- End benefits section -------->
 
-<!-------Recent listing section-------->
-@include('include.recent-listing');
-<!-------Recent listing section-------->
+  <!------- Start recent listing section ------->
+  @include('include.recent-listing')
+  <!------- End recent listing section ------->
 
-<!-------Recent listing section-------->
-@include('include.premium-listing');
-<!-------Recent listing section-------->
+  <!------- Start premium listing section ------->
+  @include('include.premium-listing')
+  <!------- End premium listing section ------->
 
-<!-------Recent listing section-------->
-@include('include.location-wise-listing');
-<!-------Recent listing section-------->
+  <!------- Start location-wise listing section ------->
+  @include('include.location-wise-listing')
+  <!------- End location-wise listing section ------->
 
-<!-------Recent listing section-------->
-@include('include.all-service-tab');
-<!-------Recent listing section-------->
+  <!------- Start all service tab section ------->
+  @include('include.all-service-tab')
+  <!------- End all service tab section ------->
 
-<!---------Show case section----------->
-@include('include.show-case');
-<!---------Show case section----------->
+  <!--------- Start showcase section ----------->
+  @include('include.show-case')
+  <!--------- End showcase section ----------->
 
-<!---------Our client section---------->
-@include('include.client');
-<!---------Our client section---------->
+  <!--------- Start our client section ---------->
+  @include('include.client')
+  <!--------- End our client section ---------->
 
-<!----Connect with us confidential----->
-@include('include.connect-with-us');
-<!----Connect with us confidential----->
+  <!---- Start connect with us section ----->
+  @include('include.connect-with-us')
+  <!---- End connect with us section ----->
 
+  <!-------- Start footer section --------->
+  @include('include.footer')
+  <!-------- End footer section --------->
 
-<!--------Start footer section--------->
-@include('include.footer');
-<!--------End footer section--------->
-
-
-<!------js------>
-@include('include.js');
+  <!------ Include JavaScript Files ------>
+  @include('include.js')
 
 </body>
 </html>
