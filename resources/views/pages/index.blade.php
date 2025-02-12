37bc2dcd7870f9500b700e12d54082cf830ee93d
Guddu Kumar Ray <gudduray9869@gmail.com>
2025-02-12 15:29:25 +0530

    index created

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  @include('include.css');
+</head>
+<body>
+  
+  <!-------Start header section------->  
+ @include('include.header');
+<!-------Emd header section-\------>
+
+<!------Star home banner section------->
+@include('include.home-banner');
+<!------End home banner section------->
+
+<!------Start buy business section------->
+@include('include.buy-business');
+<!------End buy business section------->
+
+<!--------Start benefits section------->
+@include('include.benefits');
+<!--------End benefits section--------->
+
+<!-------Recent listing section-------->
+@include('include.recent-listing');
+<!-------Recent listing section-------->
+
+<!-------Recent listing section-------->
+@include('include.premium-listing');
+<!-------Recent listing section-------->
+
+<!-------Recent listing section-------->
+@include('include.location-wise-listing');
+<!-------Recent listing section-------->
+
+<!-------Recent listing section-------->
+@include('include.all-service-tab');
+<!-------Recent listing section-------->
+
+<!---------Show case section----------->
+@include('include.show-case');
+<!---------Show case section----------->
+
+<!---------Our client section---------->
+@include('include.client');
+<!---------Our client section---------->
+
+<!----Connect with us confidential----->
+@include('include.connect-with-us');
+<!----Connect with us confidential----->
+
+
+<!--------Start footer section--------->
+@include('include.footer');
+<!--------End footer section--------->
+
+
+<!------js------>
+@include('include.js');
+
+</body>
+</html>
